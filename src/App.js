b82246c29eb007585626fbc3e6ea9e43f952696d
tech-kazuki <uekazuki0411@akiyamakazukinoMacBook-puro.local>
2019-05-10 21:00:39 +0900

    Tanosii

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 
-function App() {
-  // const greeting = "React!!"
-  // const dom = <h1 className="hoge">{greeting}</h1>
+const App = () => {
+  const profiles = [
+    { name: "Kazuki", age: 25 },
+    { name: "Koki", age: 22 },
+    { name: "NoName"}
+  ]
   return (
-  <React.Fragment>
-    <label htmlFor="foo">bar</label>
-    <input type="text" onChange={() => {console.log("Hello!")}} />
-  </React.Fragment>
+    <div>
+      {
+        profiles.map((profile, index) =>{
+          return <User name={profile.name} age={profile.age} key={index}/>
+        })
+      }
+    </div>
   )
 }
 
-export default App;
+const User = (props) => {
+  return <div>Hello, I am {props.name}, and {props.age} years old!</div>
+}
+
+User.defaultProps = {
+  age: 1
+}
+
+export default App; //Appを出力してね！
