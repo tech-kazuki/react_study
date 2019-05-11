d5d10cb86a0fd0fc961f7bf2dfce91846622ab8c
tech-kazuki <uekazuki0411@akiyamakazukinoMacBook-puro.local>
2019-05-11 18:33:06 +0900

    Complete!

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,10 @@
 export const INCREMENT = 'INCREMENT'
 export const DECREMENT = 'DECREMENT'
 
-export const incremant = () => ({
-  type: 'INCREMENT'
+export const increment = () => ({
+  type: INCREMENT
 })
 
-export const decremant = () => ({
-  type: 'DECREMENT'
+export const decrement = () => ({
+  type: DECREMENT
 })
