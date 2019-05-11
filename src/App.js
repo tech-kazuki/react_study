82feeb028ddbc11d1f1a00c304966fe4ff7fde49
tech-kazuki <uekazuki0411@akiyamakazukinoMacBook-puro.local>
2019-05-11 15:00:55 +0900

    react_aaa

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const App = () => {
   const profiles = [
     { name: "Kazuki", age: 25 },
     { name: "Koki", age: 22 },
-    { name: "NoName"}
+    { name: "NoName", }
   ]
   return (
     <div>
@@ -21,8 +22,13 @@ const User = (props) => {
   return <div>Hello, I am {props.name}, and {props.age} years old!</div>
 }
 
-User.defaultProps = {
-  age: 2
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
+
+// User.defaultProps = {
+//   age: 1
+// }
 
 export default App; //Appを出力してね！
