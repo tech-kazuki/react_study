0ec5172025b6f8c71751c2997c67e4332a744e23
tech-kazuki <uekazuki0411@akiyamakazukinoMacBook-puro.local>
2019-05-11 16:03:50 +0900

    Aaa

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+export const incremant = () => ({
+  type: 'INCREMENT'
+})
+
+export const decremant = () => ({
+  type: 'DECREMENT'
+})
